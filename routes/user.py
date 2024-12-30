428ea45306911ec0f21086344d34fe5341f15514
r0manb <romanse.b27@gmail.com>
2024-12-30 23:16:38 +0500

    Изменил роут 'api/user/history' под пагинацию

--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,5 @@
 from flask import request
+from psycopg2.extras import RealDictCursor
 
 from middlewares.auth import auth_middleware
 from exceptions.exception_handler import exception_handler
@@ -6,13 +7,15 @@ from exceptions.exception_handler import exception_handler
 
 def init(app, database):
 
+    ITEMS_ON_PAGE = 10
+
     @app.route("/api/user/history", methods=["GET"])
     @auth_middleware
     def get_history():
         try:
             user_id = request.user["user_id"]
-
-            cursor = database.cursor()
+            page = int(request.args.get("page") or 0)
+            cursor = database.cursor(cursor_factory=RealDictCursor)
             cursor.execute(
                 """
                 SELECT category_id,
@@ -22,15 +25,28 @@ def init(app, database):
                 FROM history
                 WHERE user_id = '%s'
                 ORDER BY created_at DESC
+                LIMIT %s
+                OFFSET %s
+                """,
+                (user_id, ITEMS_ON_PAGE, ITEMS_ON_PAGE * page),
+            )
+            history = cursor.fetchall()
+
+            cursor.execute(
+                """
+                SELECT COUNT(*)
+                FROM history
+                WHERE user_id = '%s'
                 """
                 % user_id
             )
-            result = cursor.fetchall()
+            total_count = cursor.fetchone()['count']
 
-            columns = tuple(desc[0] for desc in cursor.description)
-            history = [dict(zip(columns, row)) for row in result]
-
-            return history, 200
+            return {
+                "total_count": total_count,
+                "items_on_page": ITEMS_ON_PAGE,
+                "history": history,
+            }, 200
         except Exception as ex:
             print(repr(ex))
             return exception_handler(ex)
