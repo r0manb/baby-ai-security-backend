2c13399e7b3a9a8676351f30a820910f4027db1b
r0manb <romanse.b27@gmail.com>
2025-01-06 14:14:31 +0500

    Рефакторинг: обернул основной код app.py в функцию main

--- app.py
+++ app.py
@@ -10,29 +10,32 @@ from dotenv import load_dotenv
 import routes
 
 
-load_dotenv()
+def main():
+    load_dotenv()
 
-try:
-    db_conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-    )
-    redis_cache = redis.Redis(
-        host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), db=0
-    )
-except Exception as ex:
-    print("Ошибка базы данных:", repr(ex))
-    sys.exit()
+    try:
+        db_conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+        )
+        redis_cache = redis.Redis(
+            host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), db=0
+        )
+    except Exception as ex:
+        print("Ошибка базы данных:", repr(ex))
+        sys.exit()
 
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    CORS(app)
+    app.config["WTF_CSRF_ENABLED"] = False
 
-CORS(app)
-app.config["WTF_CSRF_ENABLED"] = False
+    routes.init(app, db_conn, redis_cache)
 
-routes.init(app, db_conn, redis_cache)
+    app.run(debug=True)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
